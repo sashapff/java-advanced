24d84379b83e838c2f515a8549dbd711e348d384
sashapff <a.ivanova@niuitmo.ru>
2020-04-28 12:51:26 +0300

    11

--- java-solutions/ru/ifmo/rain/ivanova/bank/BankTests.java
+++ java-solutions/ru/ifmo/rain/ivanova/bank/BankTests.java
@@ -1,20 +1,20 @@
-//package ru.ifmo.rain.ivanova.bank;
-//
-//import org.junit.internal.TextListener;
-//import org.junit.runner.JUnitCore;
-//import org.junit.runner.Result;
-//
-//public class BankTests {
-//    public static void main(String[] args) {
-//        JUnitCore junit = new JUnitCore();
-//        junit.addListener(new TextListener(System.out));
-//
-//        Result result = junit.run(BankTest.class);
-//
-//        if (result.getFailureCount() > 0) {
-//            System.exit(1);
-//        }
-//        System.exit(0);
-//    }
-//
-//}
+package ru.ifmo.rain.ivanova.bank;
+
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+public class BankTests {
+    public static void main(String[] args) {
+        JUnitCore junit = new JUnitCore();
+        junit.addListener(new TextListener(System.out));
+
+        Result result = junit.run(BankTest.class);
+
+        if (result.getFailureCount() > 0) {
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+
+}
