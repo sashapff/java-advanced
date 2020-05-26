c766eba903140c3520daed781da50c0e021f30ec
sashapff <a.ivanova@niuitmo.ru>
2020-05-26 20:15:13 +0300

    12

--- java-solutions/ru/ifmo/rain/ivanova/hello/HelloUDPNonblockingClient.java
+++ java-solutions/ru/ifmo/rain/ivanova/hello/HelloUDPNonblockingClient.java
@@ -1,7 +1,6 @@
 package ru.ifmo.rain.ivanova.hello;
 
 import info.kgeorgiy.java.advanced.hello.HelloClient;
-import ru.ifmo.rain.ivanova.hello.HelloUDPUtills;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -17,102 +16,100 @@ import java.util.Iterator;
 import java.util.Objects;
 
 public class HelloUDPNonblockingClient implements HelloClient {
+    private String prefix;
+    private int requests;
+
     private class Context {
         ByteBuffer buffer;
-        int thread;
-        int request = 0;
+        private int thread;
+        private int request = 0;
 
         Context(final ByteBuffer buffer, final int thread) {
             this.buffer = buffer;
             this.thread = thread;
         }
 
-        void increase() {
+        void increaseRequests() {
             request++;
         }
-    }
 
-    private void read(final SelectionKey key, final int requests) throws IOException {
-        final DatagramChannel channel = (DatagramChannel) key.channel();
-        final Context context = (Context) key.attachment();
-        context.buffer.clear();
-        channel.receive(context.buffer);
-        context.buffer.flip();
-        String message = StandardCharsets.UTF_8.decode(context.buffer).toString();
-//        System.out.println("mes   " + message);
-        if (HelloUDPUtills.checkInts(message, context.thread, context.request)) {
-            context.increase();
-            if (context.request != requests) {
-                key.interestOps(SelectionKey.OP_WRITE);
-            } else {
-                key.channel().close();
+        void read(final SelectionKey key) throws IOException {
+            final DatagramChannel channel = (DatagramChannel) key.channel();
+            channel.receive(buffer.clear());
+            if (HelloUDPUtills.checkInts(HelloUDPUtills.decode(buffer.flip()), thread, request)) {
+                increaseRequests();
+                if (request == requests) {
+                    channel.close();
+                    return;
+                }
             }
-//            System.out.println("good" + context.request + " " + requests);
-        } else {
             key.interestOps(SelectionKey.OP_WRITE);
+        }
+
+        void write(final SelectionKey key) throws IOException {
+            final DatagramChannel channel = (DatagramChannel) key.channel();
+            channel.send(buffer.clear().put((prefix + thread + "_" + request).
+                    getBytes(StandardCharsets.UTF_8)).flip(), channel.getRemoteAddress());
+            key.interestOps(SelectionKey.OP_READ);
         }
     }
 
-    private void write(final SelectionKey key, final String prefix) throws IOException {
-        final DatagramChannel channel = (DatagramChannel) key.channel();
-        final Context context = (Context) key.attachment();
-        final String request = prefix + context.thread + "_" + context.request;
-//        System.out.println("req   " + request);
-        context.buffer.clear().put(request.getBytes(StandardCharsets.UTF_8));
-        channel.send(context.buffer.flip(), channel.getRemoteAddress());
-        key.interestOps(SelectionKey.OP_READ);
+    private void readClient(final SelectionKey key) throws IOException {
+        ((Context) key.attachment()).read(key);
+
+    }
+
+    private void writeClient(final SelectionKey key) throws IOException {
+        ((Context) key.attachment()).write(key);
     }
 
     @Override
     public void run(final String host, final int port, final String prefix, final int threads, final int requests) {
-//        try (final Selector selector = Selector.open()) {
-        Selector selector = null;
-        try {
-            selector = Selector.open();
-        } catch (IOException e) {
-            System.out.println("Can't run");
-        }
-//            System.out.println("threads" + threads);
-        for (int i = 0; i < threads; i++) {
-            try {
-                final DatagramChannel datagramChannel = DatagramChannel.open();
-                datagramChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
-                datagramChannel.configureBlocking(false);
-                datagramChannel.connect(new InetSocketAddress(InetAddress.getByName(host), port));
-                final int bufferSize = datagramChannel.socket().getReceiveBufferSize();
-                datagramChannel.register(selector, SelectionKey.OP_WRITE,
-                        new Context(ByteBuffer.allocate(bufferSize), i));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        while (!Thread.interrupted() && !selector.keys().isEmpty()) {
-            try {
-                selector.select(200);
-                if (selector.selectedKeys().isEmpty()) {
-                    for (SelectionKey key : selector.keys()) {
-                        if (key.isWritable()) {
-                            write(key, prefix);
-                        }
-                    }
-                } else {
-                    for (final Iterator<SelectionKey> i = selector.selectedKeys().iterator(); i.hasNext(); ) {
-                        final SelectionKey key = i.next();
-                        try {
-                            if (key.isWritable()) {
-                                write(key, prefix);
-                            }
-                            if (key.isReadable()) {
-                                read(key, requests);
-                            }
-                        } finally {
-                            i.remove();
-                        }
-                    }
+        this.prefix = prefix;
+        this.requests = requests;
+        try (final Selector selector = Selector.open()) {
+            for (int i = 0; i < threads; i++) {
+                try {
+                    final DatagramChannel datagramChannel = DatagramChannel.open();
+                    datagramChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
+                    datagramChannel.configureBlocking(false);
+                    datagramChannel.connect(new InetSocketAddress(InetAddress.getByName(host), port));
+                    datagramChannel.register(selector, SelectionKey.OP_WRITE,
+                            new Context(ByteBuffer.allocate(datagramChannel.socket().getReceiveBufferSize()), i));
+                } catch (IOException e) {
+                    System.out.println("Can't open DatagramChannel");
                 }
-            } catch (IOException e) {
-
             }
+            while (!Thread.interrupted() && selector.isOpen()) {
+                try {
+                    selector.select(100);
+                    if (!selector.selectedKeys().isEmpty()) {
+                        for (final Iterator<SelectionKey> i = selector.selectedKeys().iterator(); i.hasNext(); ) {
+                            final SelectionKey key = i.next();
+                            try {
+                                if (key.isReadable()) {
+                                    readClient(key);
+                                }
+                                if (key.isWritable()) {
+                                    writeClient(key);
+                                }
+                            } finally {
+                                i.remove();
+                            }
+                        }
+                    } else {
+                        for (final SelectionKey key : selector.keys()) {
+                            if (key.isWritable()) {
+                                writeClient(key);
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    System.out.println("Can't select");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Can't open Selector");
         }
     }
 
